40ef32f5bbd0636807994b285b5d052c5aa1e602
Joel Sena <joel.sena229@gmail.com>
2019-08-27 19:32:54 -0300

    Respostas-ScrollPane

--- src/Lista013Bim.java
+++ src/Lista013Bim.java
@@ -1,45 +1,77 @@
+import javax.swing.JScrollPane;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 public class Lista013Bim extends JFrame {
 	JButton jb1,jb2,jb3,jb4,jb5,jb6;
-	JLabel jl1;
+	JLabel jl1,jl7,jlt;
 	JScrollPane scroll;
 	JPanel jp;
 	public Lista013Bim() {
 		super("Lista 1 do 3º Bimestre");
-		this.setLocationRelativeTo(null);
+		ResList01_3Bim opa = new ResList01_3Bim();
+		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		jp = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		jp.setPreferredSize(new Dimension(350,230));
-		 scroll = new JScrollPane(jp, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED); 
+		jp.setPreferredSize(new Dimension(200,260));
+		add(jp);
+		scroll = new JScrollPane(jp);
+		scroll.setVerticalScrollBarPolicy ( JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED );
+		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		add(scroll);
 		jl1 = new JLabel("Escolha uma das opções: ");
 		jl1.setFont(new Font("Times new Roman",Font.BOLD, 14));
 		jp.add(jl1);
 		jb1 = new JButton("Qt01");
-		//jb1.setBackground(Color.black );
+		jb1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				opa.qt01();
+			}
+		});
 		jp.add(jb1);
-		jb2 = new JButton("Qt01");
-		//jb1.setBackground(Color.black );
+		jb2 = new JButton("Qt02");
+		jb2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				opa.qt02();
+			}
+		});
 		jp.add(jb2);
-		jb3 = new JButton("Qt01");
-		//jb1.setBackground(Color.black );
+		jb3 = new JButton("Qt03");
+		jb3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				opa.qt03();
+			}
+		});
 		jp.add(jb3);
-		jb4 = new JButton("Qt01");
-		//jb1.setBackground(Color.black );
+		jb4 = new JButton("Qt04");
+		jb4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				opa.qt04();
+			}
+		});
 		jp.add(jb4);
-		jb5 = new JButton("Qt01");
-		//jb1.setBackground(Color.black );
+		jb5 = new JButton("Qt05");
+		jb5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				opa.qt05();
+			}
+		});
 		jp.add(jb5);
-		jb6 = new JButton("Qt01");
-		//jb1.setBackground(Color.black );
+		jb6 = new JButton("Qt06");
+		jb6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				opa.qt06();
+			}
+		});
 		jp.add(jb6);
+		jl7 = new JLabel("Questão 07");
+		jl7.setFont(new Font("Consolas",Font.BOLD,15));
+		jp.add(jl7);
 		
 	}
-
 	public static void main(String[] args) {
 		Lista013Bim jan = new Lista013Bim();
-		//jan.setSize(350,230);
+		jan.setSize(300,190);
 		jan.setVisible(true);
 	}
 
---
+++ src/ResList01_3Bim.java
@@ -0,0 +1,97 @@
+import java.text.DecimalFormat;
+
+import javax.swing.JOptionPane;
+public class ResList01_3Bim {
+	public void qt01(){
+		JOptionPane.showMessageDialog(null, "O que são sementes na geração de números aleatórios? Porque um programador\n" + 
+				"pode ter interesse em definir uma semente específica?\n\nA semente aleatória é um número (ou vetor) usado para \n"
+				+ "iniciar o algoritmo gerador de números pseudo-aleatórios. Computacionalmente as sementes aleatórias são muitas vezes \n"
+				+ "geradas a partir de um valor que não se repetirá.","Questão 01", JOptionPane.INFORMATION_MESSAGE);
+	}
+	public void qt02() {
+		JOptionPane.showMessageDialog(null,"\nimport java.util.*;\n" + 
+				"public class ArrayAleatorio {\n" + 
+				"public static void main(String[] args) {\n" + 
+				"Random r = new Random();\n" + 
+				"int[] array = new int[10];\n" + 
+				"for(int i = 1; i <= 10; i++) {\n" + 
+				"array[i] = r.nextInt(100);\n" + 
+				"}\n" + 
+				"for(int i = 1; i <= 10; i++) {\n" + 
+				"System.out.print(array[i]+\" \");\n" + 
+				"}\n" + 
+				"}\n" + 
+				"}\n" + 
+				"a( ) O código não compila por falta do import da classe Random.\n" + 
+				"b( ) não existe nenhum erro neste código, portanto compilará e executar´a sem nenhuma chance de\n" + 
+				"erro.\n" + 
+				"c( ) O código não compila porque a variável i não pode ser declarada novamente no segundo for.\n" + 
+				"d(x) O código compila, mas o acesso ao elemento de ´ındice 10 do array faz o programa ser encerrado\n" + 
+				"pois este elemento não existe no array.\n" + 
+				"e( ) O código não compila porque array é uma palavra reservada do Java e não pode ser usada\n" + 
+				"como nome de vari´avel.","Questão 02",JOptionPane.INFORMATION_MESSAGE);
+	}
+	public void qt03() {
+		try {
+		int ts = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o tempo (em segundos)","Questão 03",JOptionPane.QUESTION_MESSAGE));
+		int seg = ts%60;
+		int min = (ts/60)%60;
+		int hora = ts/3600;
+		String hms = String.format("%02d:%02d:%02d", hora,min,seg);
+		JOptionPane.showMessageDialog(null,"São: "+ hms,"Questão 03",JOptionPane.QUESTION_MESSAGE);
+		}catch(Exception e) {
+			JOptionPane.showMessageDialog(null,"ERROR- Digite antes!!!","Error!!!",JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	public void qt04() {
+		try {
+		int n =2;
+		int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um número: ","Questão 04",JOptionPane.QUESTION_MESSAGE));
+		String nums = "";
+		while(num !=1) {
+			while(num%n !=0) {
+				n++;
+			}
+			if(num%n ==0) {
+				num = num/n;
+			}
+			nums += n+" ";
+		}
+		JOptionPane.showMessageDialog(null, "Os seus primos são: "+ nums, "Questão 04", JOptionPane.QUESTION_MESSAGE);
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null,"ERROR- Digite antes!!!","Error!!!",JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	public void qt05() {
+		double pi = 0;
+		for (int i = 0; i < 1000000; i++) {
+			pi += ((i%2==1)? -1.0 : 1.0)/(2.0*i+1.0);
+		}
+		String piRes = String.format("%.6f", pi*4);
+		JOptionPane.showMessageDialog(null, "Pi igual: "+piRes,"Questão 05",JOptionPane.INFORMATION_MESSAGE);
+	}
+	public void qt06() {
+		try {
+		DecimalFormat df = new DecimalFormat("#0.00");
+		int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o A da equação  =>  ax²+bx+c=0","Questão 06",JOptionPane.QUESTION_MESSAGE));
+		int b = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o B da equação  =>  "+a+"x²+bx+c=0","Questão 06",JOptionPane.QUESTION_MESSAGE));
+		int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o C da equação  =>  "+a+"x²+"+b+"x+c=0","Questão 06",JOptionPane.QUESTION_MESSAGE));
+		double del  = Math.pow(b, 2) - 4*a*c;
+		if(del<0) {
+			JOptionPane.showMessageDialog(null, "A equação "+a+"x²+"+b+"x+"+c+"=0   Não tem raíz.","Questão 06",JOptionPane.INFORMATION_MESSAGE);
+		}
+		else if(del==0) {
+			double x = -1*b/2*a;
+			JOptionPane.showMessageDialog(null, "A equação "+a+"x²+"+b+"x+"+c+"=0   Tem uma raíz.\n\nx = "+df.format(x),"Questão 06",JOptionPane.INFORMATION_MESSAGE);
+		}
+		else if(del >0) {
+			double x1 = -1*b + Math.sqrt(del)/2*a; 
+			double x2 = -1*b - Math.sqrt(del)/2*a;
+			JOptionPane.showMessageDialog(null, "A equação "+a+"x²+"+b+"x+"+c+"=0   Tem duas raízes.\n\nx1 = "+df.format(x1)+"\nx2 = "+df.format(x2),"Questão 06",JOptionPane.INFORMATION_MESSAGE);
+		}
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null,"ERROR- Digite antes!!!","Error!!!",JOptionPane.ERROR_MESSAGE);
+		}
+	
+	}
+}
